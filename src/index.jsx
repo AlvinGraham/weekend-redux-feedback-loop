692bfa8a1b481e97bf7f4bd641bf999786a6165a
Alvin Graham <grah0071@gmail.com>
2024-02-04 06:48:47 -0600

    Setup and initialize redux store instance

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './components/App/App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./components/App/App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import logger from "redux-logger";
+
+function feedbackList(state = [], action) {
+  // actions
+}
+
+// define store instance
+const feedbackStore = createStore(
+  combineReducers({ feedbackList }, applyMiddleware(logger))
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={feedbackStore}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
